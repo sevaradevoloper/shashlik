9289b9d82d8f2cb2889e43d19074475775685a37
sevaradeveloper <sevara123@icloud.com>
2025-09-12 21:05:21 +0300

    qolgani

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-function toggleMenu(id, el){
-  const menu = document.getElementById(id);
-  menu.style.display = (menu.style.display==='flex')?'none':'flex';
-  el.classList.toggle("active");
+// Bars menyu ochib yopish
+const bars = document.getElementById("bars");
+const menu = document.getElementById("menu");
+
+bars.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+// Slideshow
+let slides = document.querySelectorAll(".hero__image .slide");
+let index = 0;
+
+function changeSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
 }
+setInterval(changeSlide, 3000);
